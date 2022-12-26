ae831ab402752baecf779bd53effe35aba247de2
nmak1 <nmak1@rambler.ru>
2022-12-26 09:13:50 +0300

    Initial commit

---
+++ src/main/kotlin/User.kt
@@ -0,0 +1,10 @@
+class User(_name: String, _age: Int, _email: String){
+    val name = _name
+    var age = _age
+    val emeil = _email
+
+    fun likes (tap : Boolean) : Boolean {
+        return tap
+    }
+}
+
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,7 @@
+
+
+fun main() {
+    println("Сколько лайков?")
+    val likes = readLine()?.toInt()
+    println("Понравилось ${likes?.let { LikesCalc.getPeopleString(it) }}")
+}
